b7cd8a61c2442765c378312e5d2a98601ef41227
Alden <aj.molina092@gmail.com>
2019-01-16 14:22:24 -0500

    initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            while (num < 100)
+            {
+                num++;
+                if (num % 3 == 0 && num % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                    continue;
+                }
+                else if (num % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    continue;
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    continue;
+                }         
+                else
+                {
+                    Console.WriteLine(num);
+                    continue;
+                }
+            }
+        }
+    }
+}
